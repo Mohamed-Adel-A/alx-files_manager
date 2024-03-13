c7819bbacf0ea2416110ce971cdd242517686e04
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-13 23:10:38 +0200

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -41,17 +41,17 @@ const UsersController = {
   async getMe(req, res) {
     const token = req.headers['X-Token'];
     if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized no token' });
     }
 
     const userId = await redisClient.get(`auth_${token}`);
     if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized no userid' });
     }
 
     const user = await dbClient.client.db().collection('users').findOne({ _id: userId });
     if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized no user' });
     }
 
     return res.status(200).json({ id: user._id, email: user.email });
