aa0dba86da66bec0bfe9b3569c2270beb747cc43
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-13 23:23:11 +0200

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -33,7 +33,7 @@ const AuthController = {
 
   // sign-out the user based on the token
   async getDisconnect(req, res) {
-    const token = req.headers['X-Token'];
+    const token = req.headers['x-token'];
     console.log(token);
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized no token' });
