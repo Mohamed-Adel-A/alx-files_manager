057a1e683ed3e074b03e508715a80065598f6b1f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-14 01:42:24 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,7 @@ router.get('/users/me', UsersController.getMe);
 
 // FileController endpoints
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 export default router;
