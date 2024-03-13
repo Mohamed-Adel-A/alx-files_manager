3851acf82986790bc2af0964434e1c9bcc11263c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-13 18:50:54 +0200

    Add getMe method

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -36,6 +36,25 @@ const UsersController = {
     // Return the new user with status code 201
     return res.status(201).json({ id: result.insertedId, email: newUser.email });
   },
+
+  async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.client.db().collection('users').findOne({ _id: userId });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  },
 };
 
 export default UsersController;
