f5afeacb537855c0f113e32f9d5e3266e73da357
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-13 18:49:39 +0200

    Add "Authenticate a user" endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -10,5 +11,10 @@ router.get('/stats', AppController.getStats);
 
 // UserController endpoints
 router.post('/users', UsersController.postNew);
+
+// Authenticate a user endpoints
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
