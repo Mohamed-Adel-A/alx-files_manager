4bdd141861b09fe33fb4d31e3feb320fd768cd52
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-13 17:47:28 +0200

    Create AppController.js

---
+++ controllers/AppController.js
@@ -0,0 +1,28 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const AppController = {
+  async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+
+    return res.status(200).json(
+      {
+        redis: redisStatus,
+        db: dbStatus,
+      });
+  },
+
+    async getStats(req, res) {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+
+      return res.status(200).json(
+        {
+          users: usersCount,
+          files: filesCount
+        });
+  },
+};
+
+export default AppController;
