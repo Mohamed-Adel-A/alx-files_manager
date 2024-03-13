bb994d2f8c3da07b1879cd289a40b21df7ec3d9f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-13 13:28:26 +0200

    Create redis.js

---
+++ utils/redis.js
@@ -0,0 +1,48 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+
+class RedisClient {
+  /**
+   * create new clint
+   * any error of the redis client
+   * will be displayed in the console
+   */
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error.toString()}`);
+    });
+  }
+
+  /**
+   * check if clint connected
+   */
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Set key and value
+   */
+  async set(redisKey, redisValue, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(redisKey, redisValue);
+    await this.client.expire(redisKey, time);
+  }
+  
+
+  /**
+   * Get value by key
+   */
+  async get(redisKey) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const redisValue = await redisGet(redisKey);
+    return redisValue;
+  }
+
+  
+  }
