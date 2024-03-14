bd054db567d62915fde6ce5cdddf3b8467768b99
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-14 02:39:47 +0200

    Update FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -110,7 +110,15 @@ const FilesController = {
   },
 
   async getIndex(request, response) {
-    const user = await FilesController.getUser(request);
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      const user = await users.findOne({ _id: idObject });
+    }
+
     if (!user) {
       return response.status(401).json({ error: 'Unauthorized' });
     }
