c96b606a8c0b7097a445e445b6c8139f98073129
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-14 02:53:35 +0200

    Update FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,8 +3,8 @@ import { promises as fs } from 'fs';
 import { ObjectID } from 'mongodb';
 import mime from 'mime-types';
 import Queue from 'bull';
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import { dbClient } from '../utils/db';
+import { redisClient } from '../utils/redis';
 
 const fileQueue = new Queue('fileQueue', 'redis://127.0.0.1:6379');
 
